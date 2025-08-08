d0d9fd587e514045e251387e1c1dcecbdac66904
dostonlv <dostonlv@icloud.com>
2025-08-08 16:58:55 +0500

    add grpc methods

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"net"
+	"time"
 
 	"github.com/delta-team-uz/grpc-web-todo/storage"
 	"github.com/delta-team-uz/grpc-web-todo/todo_service_grpc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -16,10 +20,23 @@ func main() {
 		log.Fatal("Error while listening: %w", err)
 	}
 
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(
+		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
+			PermitWithoutStream: true,
+		}),
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			Time:    time.Minute,
+			Timeout: 5,
+		}),
+		grpc.MaxRecvMsgSize(10*1024*1024), // 10MB
+		grpc.MaxSendMsgSize(10*1024*1024), // 10MB
+	)
 
+	reflection.Register(grpcServer)
 	todo_service_grpc.RegisterTodoServiceServer(grpcServer, todoService)
+	slog.Info("grpc server running port :5050 ")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal("Error while listening: %w", err)
+	}
 
-	log.Println("gRPC server listening on :5050")
-	grpcServer.Serve(lis)
 }
